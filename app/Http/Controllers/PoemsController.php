1bcff4d387c52964e33067afe9538384ee107192
gnems <g_nemsveridze@cu.edu.ge>
2018-11-17 23:11:11 +0400

    v1.0

---
+++ app/Http/Controllers/PoemsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PoemsController extends Controller
+{
+    public function getPoem($id){
+
+        $poems = DB::table('poems')->where('id', '=', $id)->get();
+
+        return $poems;
+    }
+}
